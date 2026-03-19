8ee68831e5f52abe3029db1862ed6734bece03f1
rasdehya <ranselme.web@gmail.com>
2026-03-20 00:58:58 +0100

    change and delete files

--- push2talk.sh
+++ push2talk.sh
@@ -3,7 +3,6 @@
 
 export LD_LIBRARY_PATH=/home/remi/scripts/push2talk/.venv/lib/python3.12/site-packages/nvidia/cublas/lib:/home/remi/scripts/push2talk/.venv/lib/python3.12/site-packages/nvidia/cudnn/lib:$LD_LIBRARY_PATH
 
-#export LD_LIBRARY_PATH=/home/remi/scripts/push2talk/.venv/lib/python3.12/site-packages/nvidia/cublas/lib:$LD_LIBRARY_PATH
 
 cd $HOME/scripts/push2talk/
 source .venv/bin/activate
